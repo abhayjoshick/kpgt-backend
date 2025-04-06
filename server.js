81de679d8d1dec89cee243223a3a70c06dc87d5e
abhayjoshick <abhay.joshi@cloudkeeper.com>
2025-04-06 20:26:20 +0530

    changed dataset

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import apiRoutes from "./routes/api.js";
 
 const app = express();
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", apiRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Mock API server running at http://localhost:${PORT}`);
 });
